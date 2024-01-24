4d468d6f71e683ca8139cce6d87938656c43a0b5
amar-h <56476929+amar-h@users.noreply.github.com>
2024-01-24 15:20:41 +0100

    update dev_gui (auth)

--- dev_gui.py
+++ dev_gui.py
@@ -118,4 +118,4 @@ with gr.Blocks() as demo:
     
 if __name__ == "__main__":
     demo.queue()
-    demo.launch(server_name="0.0.0.0", server_port=5000)
+    demo.launch(auth=("sntUser", "snt2024Passw"), server_name="0.0.0.0", server_port=5000)
