425328ef4bc3d2aeaa4182ea9823273acd189f2a
amar-h <56476929+amar-h@users.noreply.github.com>
2024-06-18 09:57:28 +0200

    Changes in passage_object.py

--- passage_object.py
+++ passage_object.py
@@ -1,8 +1,7 @@
 class PassageObject:
-    # TODO  replace document_id with legisature_id. tips :  use a regex or add it when you execute the
-    #  Annals_datas_query.sql script into the database
+    
     def __init__(self, document_id, passage_title, page, date,  passage_text, language, leg, num_leg):
-        self.passage_text = passage_text
+        self.page_content = passage_text
         self.metadata = {'document_id': document_id,
                          'page': page,
                          'date': date,
@@ -13,7 +12,7 @@ class PassageObject:
 
     def __str__(self):
         return (f'PassageObject: {self.metadata["passage_title"]}'
-                f' on page {self.metadata["page"]}, at date {self.metadata["date"]} ,text: {self.passage_text}'
+                f' on page {self.metadata["page"]}, at date {self.metadata["date"]} ,text: {self.page_content}'
                 )
 
 
