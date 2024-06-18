c7699bde47ac1203b19cd414606b20baa40a88b0
amar-h <56476929+amar-h@users.noreply.github.com>
2024-06-18 09:56:19 +0200

    Changes in global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -1,20 +1,36 @@
-from models import SentenceBERTEmbedding
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceEmbeddings
-from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from FlagEmbedding import FlagReranker
+from enum import Enum
+#from models import *
 
 '''
     This file contains the global variables that are used in the project
 '''
-# 'SVD_for_documents_retrieval'
-# 'SVD_for_documents_retrieval_mpnet_v2'
-collection_name = 'coll_bge_3m_full'
+
+
+class Mode(Enum):
+    host = 'host'
+    local = 'local'
+
+
+collection_name_bge = 'coll_bge_3m_full'
+collection_name_all_mini = 'new_all_miniLM_L6_coll'
 local_collection_path = 'chromadb_data_snt'
+
 host = '172.16.13.74'
 port_number = '8000'
+
 embedder = HuggingFaceBgeEmbeddings(
-    model_name="BAAI/bge-m3", model_kwargs={"device": "cpu"}, query_instruction="Represent the query for passage retrieval: "
-)
-embeddings_all_MiniLM_L6_V2 = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-cross_encoder = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-large")
+    model_name="BAAI/bge-m3",
+    model_kwargs={"device": "cpu"},
+    query_instruction="Represent the query for passage retrieval: "
+    )
+"""CustomHuggingFaceBgeEmbeddings()"""
+
+embeddings_all_MiniLM_L6_V2 = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2",
+    model_kwargs={'device': 'cpu'}
+    )
+"""CustomHuggingFaceEmbeddings()"""
+
 reranker = FlagReranker("BAAI/bge-reranker-v2-m3", use_fp16=True)
