af32c61bd403d24ead606ab508dbeaa0fba16f4d
amar-h <56476929+amar-h@users.noreply.github.com>
2024-06-18 09:57:00 +0200

    Changes in models.py

--- models.py
+++ models.py
@@ -1,39 +1,23 @@
-import torch
-from sentence_transformers import SentenceTransformer
-from transformers import BertTokenizer, BertModel
-from chromadb import EmbeddingFunction, Embeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceEmbeddings
 
 
-# TODO: Finetune BERT or S-BERT model for senate documents
-
-#all-MiniLM-L6-v2 (fast but less efficient, the default one)
-#all-mpnet-base-v2
-class SentenceBERTEmbedding(EmbeddingFunction):
-    def __init__(self, model_name='BAAI/bge-m3'):
-        # initialize tokenizer and model
-        self.model = SentenceTransformer(model_name, device='cuda' if torch.cuda.is_available() else 'cpu')
-
-    def __call__(self, texts) -> Embeddings:
-        # Generate BERT embeddings
-        with torch.no_grad():
-            outputs = self.model.encode(texts, convert_to_tensor=True)
-        return outputs.detach().tolist()
+class CustomHuggingFaceEmbeddings(HuggingFaceEmbeddings):
+    def __call__(self, input: str) -> list[float]:
+        model_name = "all-MiniLM-L6-v2"
+        model_kwargs = {'device': 'cpu'}
+        self.model = HuggingFaceEmbeddings(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+        )
+        return self.model.embed_query(input)
 
 
-class BERTEmbedding(EmbeddingFunction):
+class CustomHuggingFaceBgeEmbeddings(HuggingFaceBgeEmbeddings):
+    def __call__(self, input: str) -> list[float]:
+        self.model = HuggingFaceBgeEmbeddings(
+            model_name="BAAI/bge-m3",
+            model_kwargs={"device": "cpu"},
+            query_instruction="Represent the query for passage retrieval: "
+        )
+        return self.model.embed_query(input)
 
-    def __init__(self, model_name='bert-base-multilingual-cased'):
-        # initialize tokenizer and model
-        self.tokenizer = BertTokenizer.from_pretrained(model_name)
-        self.model = BertModel.from_pretrained(model_name)
-
-    def __call__(self, texts) -> Embeddings:
-        # Tokenize the input texts
-        inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
-
-        # Generate BERT embeddings
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-        outputs = outputs[0][:, 0, :]
-
-        return outputs.detach().tolist()[0]
