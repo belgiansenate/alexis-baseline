e3c86d0229d649d03de9177d9fdfed28cde6819a
amar-h <56476929+amar-h@users.noreply.github.com>
2024-06-18 09:59:22 +0200

    Changes in store_passages.py

--- store_passages.py
+++ store_passages.py
@@ -1,15 +1,17 @@
 from arguments import parse_args
 from database_operations import passages_storing
-from vector_database_manager import Mode, ChromaClient
+from global_variables import local_collection_path, Mode
+
+import chromadb
 
 if __name__ == '__main__':
     args = parse_args()
     if args.mode == Mode.local:
-        client = ChromaClient(mode=Mode.local, path_directory='chromadb')
+        client = chromadb.PersistentClient(path=local_collection_path)
     else:
-        client = ChromaClient(host=args.host, mode=Mode.host, port_number=args.port)
+        client = chromadb.HttpClient(host=args.host, port=args.port)
 
-    client.get_or_create_collection(args.collection)
     passages_storing(args.path, chromadb_client=client, collection_name=args.collection,
-                     embedding_function=args.embedding,records_limit=args.limit
+                     embedding_function=args.embedding, records_limit=args.limit
                      )
+
