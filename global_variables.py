6f98b6bd04e3660d1de16c0fcd5c5f464c80a6e3
Amar HAMOUMA <56476929+amar-h@users.noreply.github.com>
2024-06-20 15:36:41 +0200

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -1,17 +1,14 @@
+"""
+    This file contains the useful global variables in the project
+"""
+
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceEmbeddings
 from FlagEmbedding import FlagReranker
 from enum import Enum
-#from models import *
-
-'''
-    This file contains the global variables that are used in the project
-'''
-
 
 class Mode(Enum):
     host = 'host'
     local = 'local'
-
 
 collection_name_bge = 'coll_bge_3m_full'
 collection_name_all_mini = 'new_all_miniLM_L6_coll'
@@ -25,12 +22,10 @@ embedder = HuggingFaceBgeEmbeddings(
     model_kwargs={"device": "cpu"},
     query_instruction="Represent the query for passage retrieval: "
     )
-"""CustomHuggingFaceBgeEmbeddings()"""
 
 embeddings_all_MiniLM_L6_V2 = HuggingFaceEmbeddings(
     model_name="all-MiniLM-L6-v2",
     model_kwargs={'device': 'cpu'}
     )
-"""CustomHuggingFaceEmbeddings()"""
 
 reranker = FlagReranker("BAAI/bge-reranker-v2-m3", use_fp16=True)
